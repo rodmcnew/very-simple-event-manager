43479318a9a72f177f0b0119d16ff9af2c6343f9
Rod McNew <rmcnew@relivinc.com>
2017-03-28 10:52:14 -0500

    Update and rename README.md to very-simple-event-manager.js

--- README.md
+++
@@ -1,2 +0,0 @@
-# very-simple-event-manager
-A very simple event manager written in ES5
---
+++ very-simple-event-manager.js
@@ -0,0 +1,30 @@
+function verySimpleEventManager() {
+    var handlers = {};
+    /**
+     * Add a listener
+     *
+     * @param {string} eventName The event's name
+     * @param {function} handler a function that accepts an optional 'event-data' object as its first argument
+     */
+    this.on = function (eventName, handler) {
+        if (!handlers[eventName]) {
+            handlers[eventName] = []
+        }
+        handlers[eventName].push(handler);
+    };
+
+    /**
+     * Trigger an event
+     *
+     * @param {string} eventName The event's name
+     * @param {object} [eventData] Optional object containing extra data for the event. Is passed to handler functions
+     */
+    this.trigger = function (eventName, eventData) {
+        if (!handlers[eventName]) {
+            return;
+        }
+        handlers[eventName].forEach(function (handler) {
+            handler(eventData);
+        });
+    }
+}
